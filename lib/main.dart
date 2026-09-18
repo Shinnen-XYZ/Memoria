7f956337286057f56d4cbdfd9579b5405e38d35c
Shinnen-XYZ <jefersonmencosc7@gmail.com>
2026-09-18 10:53:07 -0600

    Se cambieron emojis

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class PantallaMemoria extends StatefulWidget {
 }
 
 class _PantallaMemoriaState extends State<PantallaMemoria> {
-  final List<String> _simbolos = ['🐶', '🐱', '🐵', '🦊', '🐸', '🐼', '🦁', '🐷'];
+  final List<String> _simbolos = ['🦞', '🐠', '🐉', '🦀', '🐸', '🐼', '🦁', '🦍'];
   late List<String> _cartas;
   late List<bool> _volteadas;
   late List<bool> _emparejadas;
